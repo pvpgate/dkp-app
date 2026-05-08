0dd055d68a27133f454952b576dde504945aec6a
Roman <roman@MacBook-Air-Roman.local>
2026-05-08 16:59:57 +0300

    create table users

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
+import os
+import psycopg2
 
 app = FastAPI()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+conn = psycopg2.connect(DATABASE_URL)
+cur = conn.cursor()
 
 app.add_middleware(
     CORSMiddleware,
