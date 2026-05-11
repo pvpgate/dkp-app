67072a245742c3526a49cf6d080814fdceb6831e
Roman <roman@MacBook-Air-Roman.local>
2026-05-11 13:29:41 +0300

    add manage button

--- frontend/src/pages/ClanPage.jsx
+++ frontend/src/pages/ClanPage.jsx
@@ -20,13 +20,25 @@ function ClanPage() {
 
   return (
     <div style={{ padding: 20 }}>
-      <h1>{clan ? clan.name : "Loading..."}</h1>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          marginBottom: 20,
+        }}
+      >
+        <h1 style={{ margin: 0 }}>
+          {clan ? clan.name : "Loading..."}
+        </h1>
 
-      <button>
-        Delete clan
-      </button>
+        <button>
+          Delete clan
+        </button>
+      </div>
 
       <h2>Members</h2>
+
       <p>Members list will be here</p>
     </div>
   );
