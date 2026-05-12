b31aeeabf01b01c5b92fc02523a5f013df89b77b
Roman <roman@MacBook-Air-Roman.local>
2026-05-12 12:02:59 +0300

    minor fixes

--- backend/routes/join_clan_request.py
+++ backend/routes/join_clan_request.py
@@ -1,5 +1,4 @@
 from fastapi import APIRouter
-from pydantic import BaseModel
 import json
 from urllib.parse import parse_qsl
 from db import conn, cur
@@ -7,32 +6,49 @@ from db import conn, cur
 router = APIRouter()
 
 
-class JoinClanRequest(BaseModel):
-    initData: str
-    publicId: str
-    gameNickname: str
-
-
 @router.post("/clans/join-request")
-async def join_clan_request(data: JoinClanRequest):
-    print("JOIN REQUEST HIT")
-    parsed = dict(parse_qsl(data.initData))
+async def join_clan_request(data: dict):
+    print("JOIN RAW DATA:", data)
+
+    init_data = data.get("initData")
+    public_id = data.get("publicId")
+    game_nickname = data.get("gameNickname")
+
+    if not init_data:
+        return {
+            "ok": False,
+            "error": "Нет данных Telegram"
+        }
+
+    if not public_id:
+        return {
+            "ok": False,
+            "error": "Введите ID клана"
+        }
+
+    if not game_nickname:
+        return {
+            "ok": False,
+            "error": "Введите игровой ник"
+        }
+
+    parsed = dict(parse_qsl(init_data))
     user_data = json.loads(parsed["user"])
     user_id = user_data["id"]
 
-    if not data.publicId.isdigit() or len(data.publicId) != 6:
+    if not public_id.isdigit() or len(public_id) != 6:
         return {
             "ok": False,
             "error": "ID клана должен состоять из 6 цифр"
         }
 
-    if not data.gameNickname.isalnum():
+    if not game_nickname.isalnum():
         return {
             "ok": False,
             "error": "Игровой ник может содержать только буквы и цифры"
         }
 
-    if len(data.gameNickname) < 2 or len(data.gameNickname) > 16:
+    if len(game_nickname) < 2 or len(game_nickname) > 16:
         return {
             "ok": False,
             "error": "Длина игрового ника может быть от 2 до 16 символов"
@@ -42,7 +58,7 @@ async def join_clan_request(data: JoinClanRequest):
     SELECT id
     FROM clans
     WHERE public_id = %s
-    """, (data.publicId,))
+    """, (public_id,))
 
     clan = cur.fetchone()
 
@@ -79,7 +95,7 @@ async def join_clan_request(data: JoinClanRequest):
     """, (
         clan_id,
         user_id,
-        data.gameNickname
+        game_nickname
     ))
 
     conn.commit()
